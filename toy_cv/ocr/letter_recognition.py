20243d8d374c741d47f41e74d05d47e5d4bd462a
gajanlee <lee_jiazh@163.com>
2019-03-27 17:15:04 +0800

    车牌识别

---
+++ toy_cv/ocr/letter_recognition.py
@@ -0,0 +1,121 @@
+import time
+import numpy as np
+from collections import namedtuple
+
+from keras.utils import np_utils
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Reshape, Dropout, Flatten
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+
+from PIL import Image
+
+Dataset = namedtuple('Dataset', ['base_dir', "classes"])
+
+WIDTH, HEIGHT = 20, 20
+
+def time_recoder(msg):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            time_start = time.time()
+            elements = func(*args, **kwargs)
+            time_elapsed = time.time()
+            print(f"{msg}%s"%time_elapsed)
+            return elements
+        return wrapper
+    return decorator
+
+def image_file_count(dir):
+    return sum(sum(1 for file in files if file.endswith(".jpg"))
+        for _, _, files in os.walk(dir))
+
+def get_image_pixels(filepath):
+    img = Image.open(filepath)
+    assert (WIDTH, HEIGHT) == img.size
+    
+    return [img.getpixel((w, h)) for w in range(20) for h in range(20)]
+    
+# Convert Image Files to Numpy Array
+def convert_image_array(base_dir, classes):
+    image_count = image_file_count(base_dir)
+    num_classes = len(classes)
+
+    image_array = np.zeros((image_count, WIDTH*HEIGHT))
+    label_array = np.zeros(image_count)
+    
+    index = 0
+    for category in range(num_classes):
+        dir = f"{base_dir}/{category}/"
+        for _, _, files in os.walk(dir):
+            for file in files:
+                image_array[index] = get_image_pixels(filepath)
+                label_array[index] = category
+                index += 1
+    return image_array, label_array, classes
+
+@time_recoder("加载图片")
+def get_task_data(task, mode="train"):
+    tasks = {
+        "area": Dataset(f"./dataset/{mode}/area", "ABCDEFGHIJKLMNOPQRSTUVWXYZ",),
+        "letter": Dataset(f"./dataset/{mode}/letter", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",),
+        "province": Dataset(f"./dataset/{mode}/province", ("皖", "沪", "津", "渝", "冀", "晋", "蒙", "辽", "吉", "黑", "苏", "浙", "京", "闽", "赣", "鲁", "豫", "鄂", "湘", "粤", "桂", "琼", "川", "贵", "云", "藏", "陕", "甘", "青", "宁", "新"))
+    }
+    x_array, y_array, classes = convert_image_array(tasks[task].base_dir, tasks[task].classes)
+    # 不除以255训练不收敛
+    x_array = x_array.reshape(-1, WIDTH, HEIGHT, 1) / 255
+    y_array = np_utils.to_categorical(y_array, num_classes=len(tasks[task].classes))
+    
+    return x_array, y_array, classes
+
+
+class SimpleModel(object):
+    
+    def __init__(self, task, classes):
+        num_classes = len(classes)
+        model = Sequential()
+        
+        # Conv1, channel 16
+        # shape: (-1, 10, 10, 16)
+        model.add(Conv2D(filters=16, kernel_size=(3, 3), strides=(1, 1), padding="same", 
+                         data_format="channels_last", activation="relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding="same"))
+        
+        # Conv2, channel 32
+        # shape: (-1, 10, 10, 32)
+        model.add(Conv2D(filters=32, kernel_size=(1, 1), strides=(1, 1), padding="same", 
+                         data_format="channels_last", activation="relu"))
+        model.add(MaxPooling2D(pool_size=(1, 1), strides=(1, 1), padding="same"))
+        
+        # Full connection
+        # shape: (-1, 512)
+        model.add(Flatten())
+        model.add(Dense(512, activation="relu"))
+        
+        # Dropout
+        model.add(Dropout(rate=0.1))
+        
+        # Output layer
+        model.add(Dense(num_classes, activation="softmax"))
+        
+        # Optimizer
+        model.compile(optimizer=Adam(lr=1e-4),
+                    loss="categorical_crossentropy",
+                    metrics=['accuracy'])
+        
+        self.model = model
+        self.task = task
+        self.classes = classes
+    
+    def fit(self, x_train, y_train, x_eval=None, y_eval=None):
+        BATCH_SIZE = 64
+        EPOCHS = 20
+        if not x_eval or not y_eval:
+            self.model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_split=0.2)
+        else:
+            self.model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(x_eval, y_eval))
+            
+    def save(self):
+        self.model.save("{}_{}".format(self.task, len(self.classes)))
+        
+    def load(self):
+        self.model.load_weights("{}_{}".format(self.task, len(self.classes)))
+        
